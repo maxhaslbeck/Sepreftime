5fee224d69605ec6db12a5e9cf259bd415c8b0ce
Maximilian P. L. Haslbeck <haslbema@in.tum.de>
2018-09-28 11:07:16 +0200

    import Augmenting_Path_BFS

---
+++ Refine_Foreach.thy
@@ -0,0 +1,50 @@
+theory Refine_Foreach
+imports Sepreftime
+begin
+
+
+text {*
+  A common pattern for loop usage is iteration over the elements of a set.
+  This theory provides the @{text "FOREACH"}-combinator, that iterates over 
+  each element of a set.
+*}
+
+subsection {* Auxilliary Lemmas *}
+text {* The following lemma is commonly used when reasoning about iterator
+  invariants.
+  It helps converting the set of elements that remain to be iterated over to
+  the set of elements already iterated over. *}
+lemma it_step_insert_iff: 
+  "it \<subseteq> S \<Longrightarrow> x\<in>it \<Longrightarrow> S-(it-{x}) = insert x (S-it)" by auto
+
+subsection {* Definition *}
+
+text {*
+  Foreach-loops come in different versions, depending on whether they have an 
+  annotated invariant (I), a termination condition (C), and an order (O).
+
+  Note that asserting that the set is finite is not necessary to guarantee
+  termination. However, we currently provide only iteration over finite sets,
+  as this also matches the ICF concept of iterators.
+*}
+   
+definition "FOREACH_body f \<equiv> \<lambda>(xs, \<sigma>). do {
+  let x = hd xs; \<sigma>'\<leftarrow>f x \<sigma>; RETURNT (tl xs,\<sigma>')
+  }"
+
+definition FOREACH_cond where "FOREACH_cond c \<equiv> (\<lambda>(xs,\<sigma>). xs\<noteq>[] \<and> c \<sigma>)"
+
+text {* Foreach with continuation condition, order and annotated invariant: *}
+
+definition FOREACHoci ("FOREACH\<^sub>O\<^sub>C\<^bsup>_,_\<^esup>") where "FOREACHoci R \<Phi> S c f \<sigma>0 inittime \<equiv> do {
+  ASSERT (finite S);
+  xs \<leftarrow> SPECT (emb (\<lambda>xs. distinct xs \<and> S = set xs \<and> sorted_wrt R xs) inittime);
+  (_,\<sigma>) \<leftarrow> whileT (*
+    (\<lambda>(it,\<sigma>). \<exists>xs'. xs = xs' @ it \<and> \<Phi> (set it) \<sigma>) *) (FOREACH_cond c) (FOREACH_body f) (xs,\<sigma>0); 
+  RETURNT \<sigma> }"
+
+text {* Foreach with continuation condition and annotated invariant: *}
+definition FOREACHci ("FOREACH\<^sub>C\<^bsup>_\<^esup>") where "FOREACHci \<equiv> FOREACHoci (\<lambda>_ _. True)"
+
+
+end
